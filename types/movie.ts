a9052537fb72e70b5080596987b05b5177646498
ayush143-maker <rahulbhartimzp7@gmail.com>
2026-08-23 11:00:13 +0530

    Add Movie interface and TMDB image base URL

---
+++ types/movie.ts
@@ -0,0 +1,5 @@
+export interface Movie {
+  id: number; title: string; poster_path: string | null; backdrop_path: string | null;
+  overview: string; vote_average: number; release_date: string; genre_ids: number[];
+}
+export const TMDB_IMAGE_BASE = "https://image.tmdb.org/t/p/w500";
